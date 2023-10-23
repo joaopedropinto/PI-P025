b003f9202b864db2957991b2a87602984e4da7db
kayquepiton <kayquepiton@gmail.com>
2023-10-23 15:53:09 -0300

    PI-P025 Kayque

--- Exercicio2.cpp
+++ Exercicio2.cpp
@@ -65,6 +65,117 @@ public:
     }
 };
 
-//INICIO DA PARTE DE KAYQUE 
+// Função principal - ponto de entrada do programa
+int main() {
+    // Criação de uma instância da rede social
+    RedeSocial redeSocial;
 
-//FIM DA PARTE DE KAYQUE
+    int opcao = 0;
+
+    // Loop principal - Menu interativo para o usuario
+    while (true) {
+        cout << "Mini Rede Social" << endl;
+        cout << "1. Registrar usuario" << endl;
+        cout << "2. Postar tweet" << endl;
+        cout << "3. Seguir usuario" << endl;
+        cout << "4. Ver feed" << endl;
+        cout << "5. Salvar dados" << endl;
+        cout << "6. Recuperar dados" << endl;
+        cout << "7. Sair" << endl;
+
+        cout << "\nSelecione uma opcao: ";
+        cin >> opcao;
+        cin.ignore();
+
+        // Processando a opcao escolhida pelo usuario
+        switch (opcao) {
+            case 1: {
+                // Registrar um novo usuario
+                string nome_usuario, nome;
+                cout << "Nome de usuario: ";
+                getline(cin, nome_usuario);
+                cout << "Nome real: ";
+                getline(cin, nome);
+                redeSocial.registrar_usuario(nome_usuario, nome);
+                cout << "usuario registrado com sucesso!\n" << endl;
+                break;
+            }
+            case 2: {
+                // Postar um novo tweet
+                string nome_usuario, tweet;
+                cout << "Nome de usuario: ";
+                getline(cin, nome_usuario);
+                Usuario* user = redeSocial.buscar_usuario(nome_usuario);
+                if (user) {
+                    cout << "Digite seu tweet: ";
+                    getline(cin, tweet);
+                    user->postar_tweet(tweet);
+                    cout << "Tweet postado!\n" << endl;
+                } else {
+                    cout << "usuario não encontrado!\n" << endl;
+                }
+                break;
+            }
+            case 3: {
+                // Seguir um usuario
+                string nome_usuario, nome_seguir;
+                cout << "Seu nome de usuario: ";
+                getline(cin, nome_usuario);
+                Usuario* user = redeSocial.buscar_usuario(nome_usuario);
+                cout << "Nome de usuario que deseja seguir: ";
+                getline(cin, nome_seguir);
+                Usuario* user_seguir = redeSocial.buscar_usuario(nome_seguir);
+                if (user && user_seguir) {
+                    user->seguir(user_seguir);
+                    cout << "Agora você está seguindo " << nome_seguir << "!\n" << endl;
+                } else {
+                    cout << "Erro ao seguir usuario!" << endl;
+                }
+                break;
+            }
+            case 4: {
+                // Ver os tweets no feed
+                string nome_usuario;
+                cout << "Nome de usuario: ";
+                getline(cin, nome_usuario);
+                Usuario* user = redeSocial.buscar_usuario(nome_usuario);
+                if (user) {
+                    vector<Tweet*> feed = user->receber_feed();
+                    cout << "--- Feed ---" << endl;
+                    for (auto& tweet : feed) {
+                        cout << tweet->get_autor()->get_nome() << ": " << tweet->get_conteudo() << endl;
+                    }
+                    cout << "\n";
+                } else {
+                    cout << "usuario não encontrado!" << endl;
+                }
+                break;
+            }
+            case 5: {
+                // Salvar os dados no arquivo
+                redeSocial.salvar_dados("dados.txt");
+                cout << "Dados salvos!\n" << endl;
+                break;
+            }
+            case 6: {
+                // Recuperar os dados do arquivo
+                redeSocial.recuperar_dados("dados.txt");
+                cout << "Dados recuperados!\n" << endl;
+                break;
+            }
+            case 7: {
+                // Sair do programa
+                return 0;
+            }
+            default: {
+                // opcao inválida
+                cout << "opcao inválida!" << endl;
+                break;
+            }
+        }
+    }
+
+    // Terminar o programa
+    return 0;
+}
+
