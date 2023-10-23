4c864fbbe1ae89325d66eedd556b142bad675215
ValberFr <143513885+ValberFr@users.noreply.github.com>
2023-10-23 04:07:44 -0300

    PI-P025 Valber

--- Exercicio2.cpp
+++ Exercicio2.cpp
@@ -25,3 +25,46 @@ public:
     time_t get_data_criacao() const { return data_criacao; }
 };
 
+//INICIO DA PARTE DE GABRIEL
+
+//FIM DA PARTE DE GABRIEL
+
+//INICIO DA PARTE DE GREGUE
+
+// FIM DA PARTE DE GREGUE
+
+    // Função para buscar um usuario pelo nome de usuario
+    Usuario* buscar_usuario(const string& nome_usuario) const {
+        for (auto& user : usuarios) {
+            if (user->get_nome_usuario() == nome_usuario) return user;
+        }
+        return nullptr;
+    }
+
+    // Getters
+    vector<Usuario*> listar_usuarios() const { return usuarios; }
+    vector<Tweet*> listar_tweets() const { return tweets; }
+
+    // Função para salvar dados em arquivo
+    void salvar_dados(const string& filename) {
+        ofstream file(filename);
+        for (auto& user : usuarios) {
+            file << user->get_nome_usuario() << " " << user->get_nome() << endl;
+        }
+        file.close();
+    }
+
+    // Função para recuperar dados de arquivo
+    void recuperar_dados(const string& filename) {
+        ifstream file(filename);
+        string nome_usuario, nome;
+        while (file >> nome_usuario >> nome) {
+            registrar_usuario(nome_usuario, nome);
+        }
+        file.close();
+    }
+};
+
+//INICIO DA PARTE DE KAYQUE 
+
+//FIM DA PARTE DE KAYQUE
